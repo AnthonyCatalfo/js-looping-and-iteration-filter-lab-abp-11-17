42d04e0cceeae6a7214663b268d2288fda430b86
Anthony Catalfo <anthonycatalfo@gmail.com>
2017-11-16 20:55:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 function findMatching(drivers, name){
  return drivers.filter(function (driver) { return name.toLowerCase()===driver.toLowerCase(); });
 }
+function fuzzyMatch(drivers, nameBeginsWith) {
+  return drivers.filter(function (driver) { return nameBeginsWith===driver.startsWith(nameBeginsWith, 0); });
+
+}
